35bb45372c94148be62bd2782b83c2769735e054
Denis Bonafe <denisgarcia.dev@gmail.com>
2020-03-29 19:16:47 -0300

    API de teste PontoInteligente

---
+++ src/main/java/com/cafeincapsula/pontointeligente/api/repositories/FuncionarioRepository.java
@@ -0,0 +1,15 @@
+package com.cafeincapsula.pontointeligente.api.repositories;
+
+import com.cafeincapsula.pontointeligente.api.entities.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+	Funcionario findByCpf(String cpf);
+	
+	Funcionario findByEmail(String email);
+	
+	Funcionario findByCpfOrEmail(String cpf, String email);
+}
